166601c89da8ba7861b95c88dd9266f1951c3d76
thirupathi-chintu <thirupathiperaboina@gmail.com>
2021-01-14 19:38:57 +0530

    Added with chnages

--- django_cache/django_cache/settings.py
+++ django_cache/django_cache/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://redis-primary:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
